104f6aa79d6679262ecf89a02145697d96dd78de
Adejobi Ezekiel <ezeko2017@gmail.com>
2020-02-24 20:47:23 +0100

    feat:
    refactor show home into controller

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,5 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
+// show home
+Route::get('/', 'HomeController@showHome' );
